be46a13c771f806947ede5c1a5b473edefe666e5
Dario Rodriguez <dariorodriguez7810@gmail.com>
2025-06-14 19:58:34 -0300

    Arreglo general de user_id "bienvenido" Re-organiza pages, con carpeta examples, para almacenar código a futuro.

--- pages/calendario.php
+++ pages/calendario.php
@@ -1,7 +1,7 @@
 <?php
 require('system/main.php');
 renderNavbar();
-
+session_start();
 $layout = new HTML(title: 'PHP via Vite');
 
 // use App\resources\Database;
@@ -16,3 +16,12 @@ $layout = new HTML(title: 'PHP via Vite');
 //     die("Error en la consulta: " . $conn->error);
 // }
 ?>
+<main class="main__content">
+    <div class="main_container">
+        <?php
+        if (!empty($_SESSION['user_id'])): echo $_SESSION['user_id'] . "Bienvenido usuario:" . $_SESSION['user_name'];
+            // unset($_SESSION['user_id']); // esto remueve la varíable, una vez se visita por primera vez (en reload se elimina)
+        endif;
+        ?>
+    </div>
+</main>
