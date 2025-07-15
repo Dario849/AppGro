2123eb2591dc0bc109bebe05c03522b292d6060b
Dario Rodriguez <dariorodriguez7810@gmail.com>
2025-07-15 15:57:54 -0300

    Importa Demo de gridstack en dashboard Only frontend Añade archivo gridstack.css para stylings E instala "npm install gridstack"

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -15,7 +15,7 @@ $usuarios = $stmt->fetchAll(); // array de filas
 <main class="main__content">
     <div class="main_container">
         <div class="main_containerDashboard">
-      <div id="alertBox" class="alertBox">
+            <div id="alertBox" class="alertBox">
                 <?php
                 if (!empty($_SESSION['error'])):
                     alertBox($_SESSION['error'], null);
@@ -26,7 +26,7 @@ $usuarios = $stmt->fetchAll(); // array de filas
                     unset($_SESSION['success']);
                 endif;
                 ?>
-      </div>
+            </div>
 
             <?php
             if (!empty($_SESSION['user_id'])):
@@ -55,9 +55,87 @@ $usuarios = $stmt->fetchAll(); // array de filas
                 }
                 ?>
             </div>
+            <!-- MAPA -->
             <div class="main_containerDashboardMapa">
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+                <link rel="stylesheet" href="src\styles\gridstack.css" />
+                <script type="module" src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons/ionicons.esm.js"></script>
+                <script nomodule="" src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons/ionicons.js"></script>
+                <link href="node_modules/gridstack/dist/gridstack.min.css" rel="stylesheet"/>
+                <script src="../node_modules/gridstack/dist/gridstack-all.js"></script>
+                <style type="text/css">
+                    .grid-stack-item-removing {
+                        opacity: 0.8;
+                        filter: blur(5px);
+                    }
 
+                    .sidepanel-item {
+                        background-color: #18bc9c;
+                        text-align: center;
+                        padding: 5px;
+                        margin-bottom: 15px;
+                    }
+
+                    #trash {
+                        background-color: rgba(255, 0, 0, 0.4);
+                    }
+
+                    ion-icon {
+                        font-size: 300%;
+                    }
+                </style>
+                <h1>Advanced Demo</h1>
+                <div class="row">
+                    <div class="sidepanel col-md-2 d-none d-md-block">
+                        <div id="trash" class="sidepanel-item">
+                            <ion-icon name="trash"></ion-icon>
+                            <div>Drop here to remove!</div>
+                        </div>
+                        <div class="grid-stack-item sidepanel-item">
+                            <ion-icon name="add-circle"></ion-icon>
+                            <div>Drag me in the dashboard!</div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-10">
+                        <div class="grid-stack"></div>
+                    </div>
+                </div>
+
+                <script type="text/javascript">
+
+                    // NOTE: REAL apps would sanitize-html or DOMPurify before blinding setting innerHTML. see #2736
+                    GridStack.renderCB = function (el, w) {
+                        el.innerHTML = w.content;
+                    };
+
+                    let children = [
+                        { x: 0, y: 0, w: 4, h: 2, content: '1' },
+                        { x: 4, y: 0, w: 4, h: 4, locked: true, content: 'locked: can\'t be pushed by others, only user!<br><ion-icon name="ios-lock"></ion-icon>' },
+                        { x: 8, y: 0, w: 2, h: 2, minW: 2, noResize: true, content: '<p class="card-text text-center" style="margin-bottom: 0">Drag me!<p class="card-text text-center"style="margin-bottom: 0"><ion-icon name="hand"></ion-icon><p class="card-text text-center" style="margin-bottom: 0">...but don\'t resize me!' },
+                        { x: 0, y: 2, w: 2, h: 2, content: '5' },
+                        { x: 2, y: 2, w: 2, h: 4, content: '6' },
+                        { x: 0, y: 4, w: 2, h: 2, content: '8' },
+                        { x: 4, y: 4, w: 4, h: 2, content: '9' },
+                        { x: 8, y: 4, w: 2, h: 2, content: '10' },
+                    ];
+                    let insert = [{ h: 2, content: 'new item' }];
+
+                    let grid = GridStack.init({
+                        cellHeight: 70,
+                        acceptWidgets: true,
+                        removable: '#trash', // drag-out delete class
+                        children
+                    });
+                    GridStack.setupDragIn('.sidepanel>.grid-stack-item', undefined, insert);
+
+                    grid.on('added removed change', function (e, items) {
+                        let str = '';
+                        items.forEach(function (item) { str += ' (x,y)=' + item.x + ',' + item.y; });
+                        console.log(e.type + ' ' + items.length + ' items:' + str);
+                    });
+                </script>
             </div>
+            <!--FIN DE MAPA -->
         </div>
     </div>
 </main>
