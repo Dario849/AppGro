49169cd5211df774297c5036e0ec5c78f31d0358
Kenstar05 <kenstar_atomo_05@hotmail.com>
2025-09-17 21:37:18 -0300

    Añade conexión PDO a eliminar_tareas.php

--- pages/eliminar_tareas.php
+++ pages/eliminar_tareas.php
@@ -1,5 +1,6 @@
 <?php
 header('Content-Type: application/json');
+require dirname(__DIR__, levels: 2) . '\system\resources\database.php';
 
 // Recibir JSON desde fetch
 $data = json_decode(file_get_contents("php://input"), true);
@@ -24,24 +25,16 @@ $hoy = date('Y-m-d H:i:s');                  // ahora con hora
 $limite = date('Y-m-d H:i:s', strtotime("-$dias days"));
 
 // Conexión mysqli
-$conn = new mysqli("localhost", "root", "", "app_campo");
-if ($conn->connect_error) {
-    echo json_encode(["success" => false, "error" => "Error de conexión"]);
-    exit;
+try {
+    // Baja lógica usando fecha_hora_inicio
+  $stmt = $pdo->prepare("UPDATE tareas 
+                      SET baja_logica = 1 
+                      WHERE (estado = 'completada' OR estado = 'cancelada') 
+                      AND fecha_hora_inicio BETWEEN ? AND ?");
+  $stmt->execute([$limite,$hoy]);
+  $affectedRows = $stmt->rowCount();
+  echo json_encode(["success" => true, "eliminadas" => $affectedRows]);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
 }
 
-// Baja lógica usando fecha_hora_inicio
-$sql = "UPDATE tareas 
-        SET baja_logica = 1 
-        WHERE (estado = 'completada' OR estado = 'cancelada') 
-        AND fecha_hora_inicio BETWEEN ? AND ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("ss", $limite, $hoy);
-$stmt->execute();
-
-$filas = $stmt->affected_rows;
-
-$stmt->close();
-$conn->close();
-
-echo json_encode(["success" => true, "eliminadas" => $filas]);
