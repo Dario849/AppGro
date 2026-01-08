f54130548899c6420d375de5527798f7bba1e123
Dario849 <dariorodriguez7810@gmail.com>
2026-01-08 17:10:51 -0300

    Añade funcionalidad para enviar correos de notificación al soporte técnico al reportar problemas.

--- system/resources/phpMailer.php
+++ system/resources/phpMailer.php
@@ -31,4 +31,31 @@ function enviarMailRecuperacion(string $emailDestino, string $claveTemporal): vo
         $_SESSION['error'] = "No se pudo enviar el correo: " . $e->getMessage();
     }
 }
+function enviarMailSoporte($emailDestino, $subject, $body){
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->isSMTP();
+        $mail->Host = $_ENV['SMTP_HOST'];
+        $mail->isHTML(true);
+        $mail->SMTPAuth = true;
+        $mail->Username = $_ENV['SMTP_USER'];
+        $mail->Password = $_ENV['SMTP_PASS'];
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = $_ENV['SMTP_PORT'];
+
+        // Datos del mensaje
+        $mail->setFrom($_ENV['SMTP_FROM'], $_ENV['SMTP_NAME']);
+        $mail->addAddress($emailDestino);
+        $mail->CharSet = 'UTF-8';
+        $mail->Encoding = 'base64';
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+        $mail->AltBody = strip_tags($body);
+
+        $mail->send();
+    } catch (Exception $e) {
+        error_log("Error al enviar correo de soporte: {$mail->ErrorInfo}");
+    }
+}
 ?>
